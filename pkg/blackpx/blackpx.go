77d18c83c33b922f072c508e27c14547d3990b16
Gustav Racenajs <racenajs.gustavs@gmail.com>
2022-03-28 19:32:40 +0300

    Ensure bounds.Min is being used instead of hardcoded 0; add Justfile

--- pkg/blackpx/blackpx.go
+++ pkg/blackpx/blackpx.go
@@ -14,13 +14,12 @@ func Count(img image.Image, pixel pixel.Pixel) (pixels int) {
 	bounds := img.Bounds()
 	var r, g, b, a uint32
 
-	for x := 0; x < bounds.Max.X; x++ {
-		for y := 0; y < bounds.Max.Y; y++ {
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 			r, g, b, a = img.At(x, y).RGBA()
 			if pixel.Is(r, g, b, a) {
 				pixels++
 			}
-
 		}
 	}
 	return pixels
